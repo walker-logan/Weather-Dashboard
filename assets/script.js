f86fffbc68d04c83b5c46a1136cd133e22b589bb
Walker Logan <walker.logan44@gmail.com>
2023-04-24 17:21:37 -0500

    added the api

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,12 @@ var today = dayjs()
 var timeDisplayEl = document.querySelector('.current-time')
 var dateDisplayEl = document.querySelector('.current-date')
 var cities = JSON.parse(localStorage.getItem("cities")) || [];
+var apiKey = "2f5f412b8dc65ace9365b098b7f6537d"
 
+document.querySelector("button").addEventListener("click", function() {
+  var userInput = document.querySelector("input").value
+  searchWeather(userInput)
+})
 
 function displayTime() {
   var rightNow = dayjs().format('	h:mm:ss A');
@@ -21,4 +26,15 @@ setInterval(displayDate, 24 * 60 * 60 * 1000);
 function saveCity() {
   localStorage.setItem("cities", JSON.stringify(cities));
 };
+
+function searchWeather(city) {
+  var url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+  fetch(url)
+  .then(function(res) {
+    return res.json()
+  })
+  .then(function(data) {
+    console.log(data)
+  })
+}
 
